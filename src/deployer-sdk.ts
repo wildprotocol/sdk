119913a8464a3a4b993e61cfb40583fb088fd7c4
Kumar Abhinav <97421185+0xabhinav@users.noreply.github.com>
2025-06-23 09:00:07 +0530

    Bugfix

--- src/deployer-sdk.ts
+++ src/deployer-sdk.ts
@@ -1,7 +1,7 @@
 import type { SDKConfig } from './types';
 
 export class DeployerSDK {
-  async getDeployer(config: SDKConfig) {
+  static async getDeployer(config: SDKConfig) {
     if (config.client === 'ethers') {
       const { EthersDeployer } = await import('./clients/ethers');
       return new EthersDeployer(config);
