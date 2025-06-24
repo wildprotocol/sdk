05c9555e11438cc95ce1f057eefbc584f6730e1c
Akash-Behara <behara.akash68@gmail.com>
2025-06-24 12:40:26 +0530

    UPDATE: update ethers example

--- examples/basic-usage.ts
+++ examples/basic-usage.ts
@@ -1,7 +1,7 @@
 import { ethers, Wallet } from 'ethers';
 import { Address, DeployerSDK } from '../src';
 
-// read private key from .env
+// Read private key from .env
 const privateKey = process.env.PRIVATE_KEY;
 
 if (!privateKey) {
@@ -11,19 +11,17 @@ if (!privateKey) {
 const publicKey = new Wallet(privateKey).address;
 console.log('Public key:', publicKey);
 
-
-const deployerContractAddress = '0x8e095febb45a3c852e81599fa9e155be83b67e2c';
-const amountETHToUseForBuy1 = '0.0000001';
-const amountTokensToSell = '770'; // calculated depending in prices below
-const amountETHToUseForBuy2 = '0.000001'; // should be enough to graduate
-const protocolFeeRecipient = '0x1234567890123456789012345678901234567890';
-const testTokenAddress = "0x03DDCF4ab7bF145bCf221bE21c52c6b10C2A6BC5";
-
-const randomNumber = String(Math.floor(Math.random() * 10000));
 const rpcUrl = "https://mainnet.base.org";
-
 const provider = new ethers.JsonRpcProvider(rpcUrl);
 const signer = new Wallet(privateKey, provider);
+
+// Constant setup
+const amountETHToUseForBuy1 = '0.0000001';
+const amountTokensToSell = '770';
+const amountETHToUseForBuy2 = '0.000001';
+const protocolFeeRecipient = '0x1234567890123456789012345678901234567890';
+const testTokenAddress = "0x03DDCF4ab7bF145bCf221bE21c52c6b10C2A6BC5";
+const randomNumber = String(Math.floor(Math.random() * 10000));
 
 async function main() {
   // Initialize the SDK
@@ -31,116 +29,95 @@ async function main() {
     client: "ethers",
     network: "base-mainnet",
     rpcUrl,
-    signer, 
+    signer,
   });
 
   try {
-
-    // Check if base token is whitelisted
+    // Check whitelist
     // base token must be whitelisted to launch a token with it
     const isWhitelisted = await sdk.read.isBaseTokenWhitelisted('0x0000000000000000000000000000000000000000');
     console.log('ETH is whitelisted:', isWhitelisted);
 
-
-  // Launch a new token
+    // Launch token
     console.log('Launching new token...');
     const launchTx = await sdk.write.launchToken({
       name: 'TT_' + randomNumber,
       symbol: 'TTK_' + randomNumber,
       image: 'https://example.com/image.png',
-      creator: publicKey as Address, // Ensure this is a valid address format
-      baseToken: '0x0000000000000000000000000000000000000000', // ETH
-      teamSupply: '1000000000000000000000000', // 1M tokens (18 decimals)
-      totalSupply: '10000000000000000000000000', // 10M tokens
-      bondingCurveSupply: '5000000000000000000000', // 5K tokens
-      liquidityPoolSupply: '995000000000000000000000', // 995K tokens
+      creator: publicKey as Address,
+      baseToken: '0x0000000000000000000000000000000000000000',
+      teamSupply: '1000000000000000000000000',
+      totalSupply: '10000000000000000000000000',
+      bondingCurveSupply: '5000000000000000000000',
+      liquidityPoolSupply: '995000000000000000000000',
       bondingCurveBuyFee: '250', // 2.5% (250 basis points)
       bondingCurveSellFee: '250', // 2.5%
       bondingCurveFeeSplits: [
         { recipient: publicKey as Address, bps: BigInt(9500) },
-        { recipient: protocolFeeRecipient as Address, bps: BigInt(500) } // 5% to protocol fee recipient mandatory
+        { recipient: protocolFeeRecipient as Address, bps: BigInt(500) }
       ],
       bondingCurveParams: {
-        prices: ['110000000', '120000000', '130000000', '140000000', '150000000', '160000000', '170000000', '180000000', '190000000', '200000000'], // Price points
+        prices: ['110000000', '120000000', '130000000', '140000000', '150000000', '160000000', '170000000', '180000000', '190000000', '200000000'],
         numSteps: '10',
-        stepSize: '500000000000000000000' // 500 tokens per step, 500 * 10 = 5000 tokens
+        stepSize: '500000000000000000000'
       },
       allowForcedGraduation: false,
-      graduationFeeBps: '0', // 5%
+      graduationFeeBps: '0',
       graduationFeeSplits: [],
-      poolFees: 3000, // 0.3%
+      poolFees: 3000,
       poolFeeSplits: [
         { recipient: publicKey as Address, bps: BigInt(9500) },
-        { recipient: protocolFeeRecipient as Address, bps: BigInt(500) } // 5% to protocol fee recipient mandatory
+        { recipient: protocolFeeRecipient as Address, bps: BigInt(500) }
       ],
-      surgeFeeDuration: '900', // 15 minutes
-      maxSurgeFeeBps: '1000', // 10%
+      surgeFeeDuration: '900',
+      maxSurgeFeeBps: '1000',
     });
-
     console.log('Launch transaction hash:', launchTx.hash);
-    // Wait for the transaction to be confirmed, this might take a while, once complete will return the token address
+    await launchTx.wait();
 
-    // Step 1: Get buy quote
+    // Get buy quote
     console.log('Getting buy quote...');
-    const buyQuote = await sdk.read.getBuyQuote(
-      testTokenAddress, // Token address
-      amountETHToUseForBuy1 // Amount in ETH
-    );
-    console.log('Buy Quote:', {
-      amountOut: buyQuote.amountOut, // amount of tokens to receive, this is in wei
-      amountInUsed: buyQuote.amountInUsed, // amount of ETH used, this is in wei
-    });
+    const buyQuote = await sdk.read.getBuyQuote(testTokenAddress, amountETHToUseForBuy1);
+    console.log('Buy Quote:', buyQuote);
 
-    // Step 2: Buy tokens
+    // Buy tokens
     console.log('Buying tokens...');
     const buyTx = await sdk.write.buyToken({
       token: testTokenAddress,
-      amountIn: amountETHToUseForBuy1, // 1 ETH
-      amountOutMin: '0', // Minimum tokens to receive (set proper slippage)
-      to: publicKey, // Recipient address
-      value: amountETHToUseForBuy1 // ETH value to send
+      amountIn: amountETHToUseForBuy1,
+      amountOutMin: '0',
+      to: publicKey,
+      value: amountETHToUseForBuy1
     });
-    
     console.log('Buy transaction hash:', buyTx.hash);
-  
-    // Step 3: Get sell quote
-    console.log('Getting sell quote...');
-    const sellQuote = await sdk.read.getSellQuote(
-      testTokenAddress, // Token address
-      amountTokensToSell // Amount of tokens to sell
-    );
-    console.log('Sell Quote:', {
-      amountOut: sellQuote.amountOut, // amount of ETH to receive, this is in wei
-      amountInUsed: sellQuote.amountInUsed, // amount of tokens used, this is in wei
-    });
+    await buyTx.wait();
 
-    // Step 4: Sell tokens (requires token approval first)
-    console.log('Approving tokens for selling...');
-    console.log('Approving', amountTokensToSell, 'tokens for selling');
+    // Get sell quote
+    console.log('Getting sell quote...');
+    const sellQuote = await sdk.read.getSellQuote(testTokenAddress, amountTokensToSell);
+    console.log('Sell Quote:', sellQuote);
+
+    // Approve and sell
+    console.log('Approving and selling tokens...');
     const sellTx = await sdk.write.approveAndSell({
       token: testTokenAddress,
-      amountIn: amountTokensToSell, // Amount of tokens to sell
-      amountOutMin: '0', // Minimum ETH to receive (set proper slippage)
-      to: publicKey // Address that will spend the tokens (Deployer contract)
+      amountIn: amountTokensToSell,
+      amountOutMin: '0',
+      to: publicKey
     });
-    console.log('Selling tokens...');
     console.log('Sell transaction hash:', sellTx.hash);
-  
-    // Step 5: Claim fees (if you're a fee recipient)
+    await sellTx.wait();
+
+    // Claim fees
     console.log('Claiming fees...');
     const claimTx = await sdk.write.claimFee(testTokenAddress);
     console.log('Claim transaction hash:', claimTx.hash);
- 
-    // Step 6: Buy more tokens
-    console.log('Getting buy quote for more tokens...');
-    const buyQuote2 = await sdk.read.getBuyQuote(
-      testTokenAddress, // Token address
-      amountETHToUseForBuy2 // Amount in ETH
-    );
-    console.log('Buy Quote:', {
-      amountOut: buyQuote2.amountOut,
-      amountInUsed: buyQuote2.amountInUsed,
-    });
+    await claimTx.wait();
+
+    // Buy more tokens
+    console.log('Getting second buy quote...');
+    const buyQuote2 = await sdk.read.getBuyQuote(testTokenAddress, amountETHToUseForBuy2);
+    console.log('Second Buy Quote:', buyQuote2);
 
     console.log('Buying more tokens...');
     const buyTx2 = await sdk.write.buyToken({
@@ -150,14 +127,16 @@ async function main() {
       to: publicKey,
       value: amountETHToUseForBuy2
     });
-    console.log('Buy transaction hash:', buyTx2); // excess eth is returned
-    const receipt = await buyTx2.wait();
-    console.log('Transaction confirmed: ', receipt); // this will return the token receipt
-  
+    console.log('Second buy transaction hash:', buyTx2.hash);
+    await buyTx2.wait();
+
     // Graduate token
     console.log('Graduating token...');
-    const graduateTx = await sdk.write.graduateToken(testTokenAddress, true); // Allow pre-graduation
+    const graduateTx = await sdk.write.graduateToken(testTokenAddress, true);
     console.log('Graduate transaction hash:', graduateTx.hash);
+    await graduateTx.wait();
+
+    console.log('All steps completed successfully!');
   } catch (error) {
     console.error('Error:', error);
   }
